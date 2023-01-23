333dc952a7ff45a13881ce068b96fc650025cc25
Rityil <112727303+sabiyadr@users.noreply.github.com>
2023-01-23 14:45:50 +0000

    update

---
+++ prog54b.py
@@ -0,0 +1,11 @@
+# Sum and Average
+numa = int(input("What is first num: "))
+numb = int(input("What is the second num: "))
+numc = int(input("What is the third num: "))
+numd = int(input("What is the fourth num: "))
+
+Sum = numa + numb + numc + numd
+Average = Sum / 4
+
+print("Sum = ", Sum)
+print("Average = ", Average)
