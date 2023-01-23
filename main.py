1d1039a7c0784ba28152daa041410d8c0d767bab
Rityil <112727303+sabiyadr@users.noreply.github.com>
2023-01-23 14:37:39 +0000

    update

--- main.py
+++ main.py
@@ -1,1 +1,31 @@
 print("Hello World")
+# This is a single-line comment
+
+""" Arithmetic Operators
++ Add
+- Subtract
+* Multiply
+/ Divide
+// Floor Divide (Divide and round-down to nearest int)
+** Pow
+% Mod (Modulus/Remainer from Int division)
+"""
+
+""" Assignment Operators
+= Value assingment (x = y)
++= Add to (x += y is shorthand for x = x + Y)
+-= Subtract frpm (x -= is shorthand for x = x - y)
+*=
+/=
+//=
+%=
+**=
+"""
+
+""" Data Types
+int - Integer (whole number)
+float - Floating-point number (number with decimal)
+str - String (any text, in qoutes)
+bool - Boolean (either True or False only)
+chr - Character (single keyboard character, in single-qoutes)
+"""
---
+++ prog52a.py
@@ -0,0 +1,8 @@
+# lang52a - Area and Perimeter of Rectangle
+length = int(input("What is the length: "))
+width = int(input("What is the Width: "))
+area = length * width
+perim = 2 * length + 2 * width
+
+print("Area = ", area)
+print("Perimeter = " + str(perim))
