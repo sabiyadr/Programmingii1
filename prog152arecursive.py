b526343ee6eff00f15f8bd82b935965d5f16b231
Rityil <112727303+sabiyadr@users.noreply.github.com>
2023-03-28 13:44:33 +0000

    Update 28Mar23

--- prog152arecursive.py
+++ prog152arecursive.py
@@ -1,4 +1,18 @@
 import sys
 sys.setrecursionlimit(5000)
 
-def 
+def add(n):
+  if n == 3: return n
+  return n + add(n-1)
+
+def main():
+  num = int(input("Enter a number: "))
+  while num != 0:
+    addn = add(num)
+    print(f"{num}! = {addn}")
+    num = int(input("Enter a number: "))
+
+
+if __name__ == "__main__":
+  main()
+    
--- prog162arecursive.py
+++ prog162arecursive.py
@@ -2,12 +2,14 @@ def fact(n):
   if n == 1: return n # Base/Ending Case
   return n * fact(n-1) # Recursive Case
 
+def factLoop():
+
 
 def main():
   num = int(input("Enter a number: "))
   while num != 0:
     factn = fact(num)
-    print(f"{num}! = {factn}")
+    print(f"{num} != {factn}")
     num = int(input("Enter a number: "))
 
 if __name__ == "__main__":
